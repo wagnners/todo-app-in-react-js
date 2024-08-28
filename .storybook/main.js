a10c249f6f6c0405df2e98b67ebdc2fbc8a8c9a5
Wagner Luz <wagnnersousa@hotmail.com>
2024-08-28 13:13:18 -0300

    feat: storybook não subiu no commit anterior

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+
+  staticDirs: ["..\\public"],
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
+};
+export default config;
